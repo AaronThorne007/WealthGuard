60aa3983dc8df16b90d0d7ad07ef07eb5744b6f4
Aaron Thorne <aaronthorne007@icloud.com>
2026-04-11 10:46:56 -0400

    pre-deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <AppShell>{children}</AppShell>
         <Toaster richColors closeButton position="top-center" />
